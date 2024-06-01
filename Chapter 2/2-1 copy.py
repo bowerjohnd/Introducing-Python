00059c8795eadc2632789123fde6f251353109ef
John Bower <bowerjohnd@gmail.com>
2024-05-31 21:17:29 -0400

    Chapter 2, assigning values to variables and copying

---
+++ Chapter 2/2-1 copy.py
@@ -0,0 +1,22 @@
+a = 7
+print("a:", a)
+b = a
+a = 10
+print("a:", a, "b:", b)
+
+
+print(type(7))
+print(type(7) == int)
+print(isinstance(7,int))
+
+a = 7
+b = a
+print(type(a))
+print(type(b))
+print(type(58))
+print(type(99.9))
+print(type("abc"))
+
+y = 5
+x = 12 - 7
+print(x)
---
+++ Chapter 2/2-2 copy.py
@@ -0,0 +1,16 @@
+x = 5
+print(x)
+y = x
+print(y)
+x = 29
+print(x)
+print(y)
+
+a = [2, 4, 6]
+b = a
+print(a)
+print(b)
+
+a[0] = 99
+print(a)
+print(b)
