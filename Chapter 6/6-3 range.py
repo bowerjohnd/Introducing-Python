6a1c5b90daa9c22d39505c865aceae0f4e1b1a54
John Bower <bowerjohnd@gmail.com>
2024-06-06 21:57:53 -0400

    Chapter 6 - while and for loops + iteration

---
+++ Chapter 6/6-3 range.py
@@ -0,0 +1,19 @@
+for x in range(0, 3) :
+	print(x)
+
+print("")
+
+print(list(range(0,3)))
+
+print("")
+
+for x in range(2, -1, -1) :
+	print(x)
+
+print("")
+
+print(list(range(2, -1, -1)))
+
+print("")
+
+print(list( range(0, 11, 2) ))
