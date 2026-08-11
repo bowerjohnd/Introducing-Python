c1ebae05c0acd30a5ff4cf48bbe056d6e48b2af6
bowerjohnd <bowerjohnd@gmail.com>
2026-08-10 21:39:29 -0400

    Chapter 17

---
+++ Chapter 17/17-04 tcp_server.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import socket
+
+address = ('localhost', 6789)
+max_size = 1000
+
+print('Starting the server at', datetime.now())
+print('Waiting for a client to call.')
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(address)
+server.listen(5)
+
+client, addr = server.accept()
+data = client.recv(max_size)
+
+print('---')
+print('At', datetime.now(), client, 'said', data)
+print('---')
+client.sendall(b'Are you talking to me?')
+client.close()
+server.close()
+
