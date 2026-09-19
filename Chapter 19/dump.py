c3b77f1daea731f1028d768cfebaa5e6e2ae4cfc
bowerjohnd <bowerjohnd@gmail.com>
2026-09-18 22:19:31 -0400

    19-14 debug using decorators

---
+++ Chapter 19/dump.py
@@ -0,0 +1,14 @@
+# 19-14 debugging decorators
+
+def dump(func):
+    "Print input arguments and output value(s)"
+
+    def wrapped(*args, **kwargs):
+        print("Function name:", func.__name__)
+        print("Input arguments:", ' '.join(map(str, args)))
+        print("Input keyword arguments:", kwargs.items())
+        output = func(*args, **kwargs)
+        print("Output:", output)
+        
+        return output
+    return wrapped
