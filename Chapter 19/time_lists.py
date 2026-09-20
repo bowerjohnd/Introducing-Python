22b2a7cb51e923ef6ddb21a51a1adb0269bd7d17
bowerjohnd <bowerjohnd@gmail.com>
2026-09-19 23:08:53 -0400

    19-24 compare list making performances with timeit

---
+++ Chapter 19/time_lists.py
@@ -0,0 +1,16 @@
+# 19-24 comparing different ways to build a list, using timeit to measure performance
+
+from timeit import timeit
+
+def make_list_1():
+    result = []
+    for value in range(1000):
+        result.append(value)
+    return result
+
+def make_list_2():
+    result = [value for value in range(1000)]
+    return result
+
+print('make_list_1 takes', timeit(make_list_1, number=1000), 'seconds')
+print('make_list_2 takes', timeit(make_list_2, number=1000), 'seconds')
