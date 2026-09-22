8f778bf2cebf13dbe3f906f1a248b06c77fe9bf2
bowerjohnd <bowerjohnd@gmail.com>
2026-09-21 22:12:49 -0400

    20- interpreter example using pillow. Added color change for loop function (very slow) and NumPy function (nearly instant).

--- Chapter 20/pillow.py
+++ Chapter 20/pillow.py
@@ -28,6 +28,9 @@ stache = Image.open('mustache.jpg').convert('RGBA')
 stache.putalpha(200)
 #stache.show()
 
+###################################################################################################################
+#                   Color change using for loop swap, processes in over 5 minutes on entire image
+###################################################################################################################
 # not in book, isolating black part of mustache and turning the background transparent
 # modified from google ai example, suggests using numPy for much faster performace
 
@@ -68,7 +71,7 @@ def replace_color_on_image_and_transparent_everything_else(image_path, target_co
 
     return img
 
-stache = replace_color_on_image_and_transparent_everything_else(stache, (0,0,0), 100, (0,0,0,240))
+stache = replace_color_on_image_and_transparent_everything_else(stache, (0,0,0), 100, (200,100,0,240))
 
 img4 = Image.new('RGBA', img.size, (255, 255, 255, 0))
 img4.paste(img, (0, 0))
@@ -79,26 +82,49 @@ img4.paste(stache, (280, 160), mask=stache)
 
 print('=' * 20)
 
+###################################################################################################################
+#                   Color change using NumPy, processes in less than 10ms on entire image
+###################################################################################################################
 # google AI suggestion for fast numpy color swap
-# needs work
 
 import numpy as np
 
-def replace_color_with_transparent_fast(image_path, target_color, output_path):
+def replace_color_with_transparent_fast(image_path, target_color, target_range, new_color):
     start_time = time.time()
     print(f'%s\t Start time: %.4f' % (target_color, start_time))
 
-    img = Image.open(image_path).convert("RGBA")
+    if isinstance(image_path, str):
+        img = Image.open(image_path).convert("RGBA")
+    else :
+        img = image_path
+
     data = np.array(img)
     r,g,b,a = data.T
-    target_areas = (r == target_color[0]) & (g == target_color[1]) & (b == target_color[2])
-    data[..., 3][target_areas.T] = 0
+    target_areas = (r >= target_color[0]-(target_range/2)) & (r <= target_color[0]+(target_range/2)) & \
+                (g >= target_color[1]-(target_range/2)) & (g <= target_color[1]+(target_range/2)) & \
+                (b >= target_color[2]-(target_range/2)) & (b <= target_color[2]+(target_range/2))
 
-    new_img = Image.fromarray(data)
-    new_img.save(output_path, "PNG")
+    data[target_areas.T] = new_color
 
     end_time = time.time()
     print(f'%s\t End time: %.4f' % (target_color, end_time))
     print(f'%s\t Elapsed time: %.4f' % (target_color, (end_time - start_time)))
 
-replace_color_with_transparent_fast('oreilly.jpg', (0,0,0), "numpy-fast-color-change.png")
+    return Image.fromarray(data)
+
+#img5 = Image.open("oreilly.jpg").convert("RGBA")
+
+img5 = img4
+
+# change a red to a purple color
+img5 = replace_color_with_transparent_fast(img5, (200,0,0), 150, (100, 0, 100, 255))
+# change a black to a yellow color
+img5 = replace_color_with_transparent_fast(img5, (0,0,0), 150, (200, 200, 0, 255))
+# change a white to a red color
+img5 = replace_color_with_transparent_fast(img5, (255,255,255), 100, (200, 0, 0, 255))
+# change an orange to black
+img5 = replace_color_with_transparent_fast(img5, (200,100,0), 100, (0, 0, 0, 255))
+
+img5.show()
+
+#img5.save("new_image.png", "PNG")
